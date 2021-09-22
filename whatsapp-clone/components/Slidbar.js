b8b1d4fc710cea5a14c077414afcf1f46904042e
NeeteshKing21 <neeteshking21@gmail.com>
2021-09-22 23:39:22 +0530

    Add Slidebar Component

---
+++ whatsapp-clone/components/Slidbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Slidbar = () => {
+    return (
+        <div>
+            <h2>We are Slidbar component</h2>
+            <p>Here all contacts will display😃</p>
+        </div>
+    )
+}
+
+export default Slidbar
