e22ef560f203c36c19f3fbd8411f3d4201748793
NeeteshKing21 <neeteshking21@gmail.com>
2021-09-22 23:09:22 +0530

    Adding Chat.js component

---
+++ whatsapp-clone/pages/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Chat = () => {
+    return (
+        <div>
+         <h1>Chat Page</h1> 
+         <p>Here our chat will display.😃</p>  
+        </div>
+    )
+}
+<h1>Cha tPage</h1>
+export default Chat
